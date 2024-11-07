59a6e64566040b779238bb8e1be7a1a6f10ff9cf
adrifoz <adrianofadel7@gmail.com>
2024-11-07 18:16:47 -0300

    working at responsive navbar

--- src/paginas/Layout.jsx
+++ src/paginas/Layout.jsx
@@ -16,36 +16,50 @@ function Layout() {
       } else {
         setCartOpen(false);
       }}
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false); 
+    const handleMenuToggle = () => { 
+            setIsMenuOpen(!isMenuOpen); 
+            };
    
     
   return (
     <>
         <nav>
+            
             <ul id='NavContainer'>
-                <li>
-                    <Link className='navLink' to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link className='navLink' to='Productos'>Productos</Link>
-                </li>
-                <li>
-                    <Link className='navLink' to='AboutUs'>Sobre Nosotros</Link>
-                </li>
-                <li>
-                    <Link className='navLink' to='Contacto'>Contacto</Link>
-                </li>
-                <li>
-                    <Link className='navLink' onClick={modifield}>Carrito</Link>
-                    {cartOpen && <Cart />}
-                </li>
-                
-                <ul className='Login'>
+                <button className="menu-toggle" aria-label="Abrir menú" onClick={handleMenuToggle}> 
+                    <span className="bar"></span> 
+                    <span className="bar"></span> 
+                    <span className="bar"></span> 
+                </button>
+
+                <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
                     <li>
-                        <Link className='navLink' to='Login'>Iniciá Sesión</Link>
+                        <Link className='navLink' to='/'>Home</Link>
                     </li>
                     <li>
-                        <Link className='navLink' to='SignUp'>Registrate</Link>
+                        <Link className='navLink' to='Productos'>Productos</Link>
                     </li>
+                    <li>
+                        <Link className='navLink' to='AboutUs'>Sobre Nosotros</Link>
+                    </li>
+                    <li>
+                        <Link className='navLink' to='Contacto'>Contacto</Link>
+                    </li>
+                    <li>
+                        <Link className='navLink' onClick={modifield}>Carrito</Link>
+                        {cartOpen && <Cart />}
+                    </li>
+                    
+                    <ul className='Login'>
+                        <li>
+                            <Link className='navLink' to='Login'>Iniciá Sesión</Link>
+                        </li>
+                        <li>
+                            <Link className='navLink' to='SignUp'>Registrate</Link>
+                        </li>
+                    </ul>
                 </ul>
             </ul>
         </nav>
